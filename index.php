96083ec9b55c43eaef578ae4d64049a2e7306e33
Erick <erickjcruz.o@gmail.com>
2023-10-10 21:32:59 -0600

    Primera version de la pagina del formulario, donde se soolicitan los datos del usuario como su nombre, edada, contraseña y correo electronico, donde en una siguiente pagina se despliega toda la informacion ingresada.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="validaInfo.php" method="post" name="frm">
+        <p>Ingrese su nombre: <input type="text" name='nombre'/></p>
+        <p>Ingrese su edad: <input type="text" name='edad'/></p>
+        <p>Ingrese su contraseña: <input type="password" name='pw'/></p>
+        <p>Ingrese su correo electronico: <input type="text" name='correo'/></p>
+        <input type="submit" value="Enviar"/>
+
+    </form>
+</body>
+</html>
